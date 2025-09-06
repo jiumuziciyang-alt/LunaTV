56a7f875b26fa5a8d4f138df10d627ed8478fbac
jiumuziciyang-alt <jiumuziciyang@gmail.com>
2025-09-06 14:36:38 +0800

    第提交

--- next.config.js
+++ next.config.js
@@ -11,10 +11,10 @@ const nextConfig = {
   swcMinify: false,
 
   experimental: {
+    runtime: 'edge', // ✅ 全局开启 Edge Runtime
     instrumentationHook: process.env.NODE_ENV === 'production',
   },
 
-  // Uncoment to add domain whitelist
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -30,23 +30,20 @@ const nextConfig = {
   },
 
   webpack(config) {
-    // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
       rule.test?.test?.('.svg')
     );
 
     config.module.rules.push(
-      // Reapply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
         resourceQuery: /url/, // *.svg?url
       },
-      // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: { not: /\.(css|scss|sass)$/ },
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        resourceQuery: { not: /url/ },
         loader: '@svgr/webpack',
         options: {
           dimensions: false,
@@ -55,7 +52,6 @@ const nextConfig = {
       }
     );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
 
     config.resolve.fallback = {
